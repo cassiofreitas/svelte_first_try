c0e26ddab529786710e87372435c24968dd08fe3
cassiofreitas <cassiofreitas@cassiofreitas.com>
2022-03-26 19:39:20 -0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,80 @@
+// 26-03-2022
+// https://dev.to/emmanuilsb/stop-overcomplicating-web-development-try-svelte-47ln#variables-amp-reactivity
+// My next step: Bind Forwarding
+//
+
+<script lang="ts">
+	import Suggestion from './Suggestion.svelte'
+	import { onMount } from 'svelte'
+	
+	let pokemonName: string = '';
+	//Fetch from api then store the mapped names
+	let pokemonData: string[] = []
+
+	onMount(() => {
+		const setPokemanData = async(): Promise<void> => {
+			const rawPokemonData = await (
+				await fetch('https://pokeapi.co/api/v2/pokemon?limit=99')
+			).json()
+
+			pokemonData = rawPokemonData.results.map(
+			(p: { name: string; url: string }) => p.name
+			)
+		}
+		setPokemanData()	
+	})
+
+	let suggestions: string[]
+	$: suggestions = 
+		pokemonName.length > 0
+			? pokemonData.filter(
+				(name) => name.includes(pokemonName)
+				)
+			:pokemonData
+
+	let chosenPokemon: string = ''
+</script>
+
+<main>
+	{#if pokemonData && pokemonData.length > 0}
+		<h1>Chose Your Pokemon</h1>
+		<h2>Chosen Pokemon: {chosenPokemon}</h2>
+		<div>
+			<span>Search:</span>
+			<input type="text" bind:value="{pokemonName}" />
+		
+			{#each suggestions as suggestion}
+				<Suggestion
+					suggestion="{suggestion}"
+					on:chosePokemon="{(e) => {
+						chosenPokemon = e.detail.pokemon
+					}}"
+				/>
+			{/each}
+		</div>
+			{:else}
+			<h2>loading...</h2>
+		{/if}
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h2 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/Suggestion.svelte
@@ -0,0 +1,21 @@
+<!-- Suggestion.svelte -->
+<script lang="ts">
+    import { createEventDispatcher } from 'svelte'
+    export let suggestion: string
+
+    //Function to dispatch a custom event.
+    const dispatch = createEventDispatcher()
+    const chosePokemon = (): void => {
+        dispatch('chosePokemon', {
+            pokemon: suggestion
+        })
+    }
+</script>
+
+<div class="suggestion" on:click="{chosePokemon}">{suggestion}</div>
+
+<style>
+    .suggestion {
+        font-size: 1.25rem;        
+    }
+</style>
